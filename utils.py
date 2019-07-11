58dddd7d091264d8bce0ee4c651479797a599c92
RieRuuuu <930920259@qq.com>
2019-07-12 05:43:33 +0800

    fix readme

--- utils.py
+++ utils.py
@@ -1,11 +1,9 @@
-import os.path
 import time
-import json
 
 
 def log(*args, **kwargs):
     # time.time() 返回 unix time
-    # 如何把 unix time 转换为普通人类可以看懂的格式呢？
+    # 把 unix time 转换为普通格式
     format = '%H:%M:%S'
     value = time.localtime(int(time.time()))
     dt = time.strftime(format, value)
